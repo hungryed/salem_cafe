44cf9f60efbb682e795d7825d450f8c07506004d
Joe Sutton <hungryed6@gmail.com>
2013-12-24 21:04:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SalemCafe::Application.config.secret_key_base = '4089da27f4e0243d5c0e3543c17dc994ffe77e6544ddeac593a3d33d2b7ed2737926066fc7cda431b835fb752bc1eca04bc6d98fa98030816383799c9ad609ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SalemCafe::Application.config.session_store :cookie_store, key: '_salem_cafe_session'
