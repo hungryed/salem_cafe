65e00bd96c1e888c4811d281cd063b7296dbe1a6
Joe Sutton <hungryed6@gmail.com>
2014-01-12 16:32:39 -0500

    OrderTotal model now displays orders over a range of dates

--- app/models/order_total.rb
+++ app/models/order_total.rb
@@ -1,21 +1,20 @@
 class OrderTotal
 
-  def initialize(start_date, end_date)
-    @start_date = start_date
-    @end_date = end_date
+  def initialize(start_date='2014-1-1', end_date= DateTime.tomorrow)
+    @start_date = start_date.to_date
+    @end_date = end_date.to_date
   end
 
   def find_orders_in_range
-    Order.find_each do |order|
+    Order.all.find_all do |order|
       order.arrival_time.between?(@start_date, @end_date)
     end
   end
 
   class << self
     def todays_orders
-      Order.find_each do |order|
-        order.arrival_time.today?
-      end
+      orders = OrderTotal.new(Date.today, DateTime.tomorrow)
+      orders.find_orders_in_range
     end
   end
 end
---
+++ spec/models/order_total_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe OrderTotal do
+  let(:order) { FactoryGirl.create(:order) }
+
+  before(:each) do
+    Timecop.freeze(Time.local(2014,1,4,6,0,0))
+  end
+
+  after(:each) do
+    Timecop.return
+  end
+
+  it "finds orders on object creation with no dates" do
+    order
+    orders = OrderTotal.new
+    orders = orders.find_orders_in_range
+    expect(orders).to include(order)
+  end
+
+  it "finds orders within a range" do
+    order
+    orders = OrderTotal.new('2014-1-3', '2014-1-10')
+    orders = orders.find_orders_in_range
+    expect(orders).to include(order)
+  end
+
+  it "does not find orders when they are not in range" do
+    order
+    orders = OrderTotal.new('2014-1-1', '2014-1-3')
+    orders = orders.find_orders_in_range
+    expect(orders).to_not include(order)
+  end
+
+  it "finds all orders for today" do
+    order
+    expect(OrderTotal.todays_orders).to include(order)
+    Timecop.return
+    expect(OrderTotal.todays_orders).to_not include(order)
+  end
+end
