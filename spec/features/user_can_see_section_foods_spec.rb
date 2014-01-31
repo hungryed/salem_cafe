65a78e760071236233291b6ba7ea6bea5616464c
Joe Sutton <hungryed6@gmail.com>
2014-01-31 12:08:20 -0500

    fixed section foods spec to test for correct things

--- spec/features/user_can_see_section_foods_spec.rb
+++ spec/features/user_can_see_section_foods_spec.rb
@@ -10,5 +10,22 @@ feature 'user can see section foods' do
     expect(page).to have_content food.section.name
     expect(page).to have_content food.name
     expect(page).to have_content food.description
+    click_on food.name
+    expect(page).to have_content food.section.name
+    expect(page).to have_content food.name
+    expect(page).to have_content food.description
+  end
+
+  scenario 'authenticated user sees food item' do
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+
+    visit root_path
+    click_on "view_sections"
+    click_on food.section.name
+    click_on food.name
+    expect(page).to have_content food.section.name
+    expect(page).to have_content food.name
+    expect(page).to have_content food.description
   end
 end
