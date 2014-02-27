82828aa3abad5d881ed6cc5185af93e7dc75f611
Joe Sutton <hungryed6@gmail.com>
2014-02-27 13:38:02 -0500

    Workers can now set food items to inactive as well from the menu screen

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -8,8 +8,9 @@ class MenusController < ApplicationController
 
   def create
     active_foods = find_active_food_ids
+    inactive_foods = find_inactive_food_ids
     section = Section.find(params[:section_id])
-    if Food.set_food_to_active(active_foods)
+    if Food.set_food_to_active(active_foods) && Food.set_food_to_inactive(inactive_foods)
       redirect_to root_path, notice: "Menu updated successfully for #{section.name}"
     else
       redirect_to root_path, notice: "There was an error."
@@ -19,5 +20,9 @@ class MenusController < ApplicationController
   private
   def find_active_food_ids
     params[:active_foods][:foods].select { |food, value| value == "1" }.keys
+  end
+
+  def find_inactive_food_ids
+    params[:active_foods][:foods].select { |food, value| value == "0" }.keys
   end
 end
